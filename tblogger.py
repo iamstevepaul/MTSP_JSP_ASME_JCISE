98a7877039f883c39c2724c5bdcc000c14c7f837
iamstevepaul <39541194+iamstevepaul@users.noreply.github.com>
2021-11-30 11:28:32 -0500

    jsp refactor

---
+++ tblogger.py
@@ -0,0 +1,16 @@
+"""
+Author: Steve Paul 
+Date: 11/29/21 """
+
+
+from tensorboard import default
+from tensorboard import program
+
+
+# tracking_address = "logger/R1_6"
+tracking_address = "logger/R1_MLP_1"
+
+if __name__ == "__main__":
+    tb = program.TensorBoard()
+    tb.configure(argv=[None,'--logdir',tracking_address])
+    tb.main()
